d677356d287c2d0bf069b845efe19bd0dd0e8bb3
Dj <ritchey0713@gmail.com>
2019-12-10 17:28:23 -0500

    remove fluff

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+
+function App() {
+  return (
+    <div className="App ui container" style={{marginTop: "10px"}}>
+      Image finder
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
