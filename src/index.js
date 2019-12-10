74173bb0e2799124345a939696f8d3fa8067ccdb
Dj <ritchey0713@gmail.com>
2019-12-10 17:28:07 -0500

    add imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+import "./styles/styles.scss"
 import * as serviceWorker from './serviceWorker';
+
+import 'semantic-ui/dist/semantic.min.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
