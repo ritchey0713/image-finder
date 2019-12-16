a982a8a9981045826dd9ec9516c1fbdc61ee9ff9
Dj <ritchey0713@gmail.com>
2019-12-16 16:28:31 -0500

    call image list

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import unsplash from "../api/unsplash"
-
+import ImageList from "./imageList"
 
 class App extends React.Component {
   state = {
     images: []
   }
   
-
-
 onSearchSubmit = async (searchTerm) => {
   const resp = await unsplash.get("/search/photos", {
     params: {
@@ -29,7 +27,7 @@ onSearchSubmit = async (searchTerm) => {
       <div className="App ui container" style={{marginTop: "10px"}}>
         Image finder
         <SearchBar onSearchSubmit={this.onSearchSubmit}/>
-        Found: {this.state.images.length}
+        <ImageList images={this.state.images}/>
       </div>
     );
 
